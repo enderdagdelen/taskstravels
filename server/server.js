04ba4fbfaa98f9433517a5534f4e3ea96a852a89
enderdagdelen <enderdagdelen@hotmail.com>
2021-07-21 16:40:31 +0300

    a few more amendments

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'..','public')
 app.use(express.static(publicPath))
 
@@ -9,6 +11,6 @@ app.get('*', (req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'))
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is up");
 })
